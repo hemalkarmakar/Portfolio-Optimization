a54dbacf9f66e3b7fc4ced9cd9119bcff0c58ddf
Hemal Karmakar <116313075+hemalkarmakar@users.noreply.github.com>
2023-05-18 12:46:58 -0500

    Create Portfolio Optimization using Scipy.R

---
+++ Portfolio Optimization using Scipy.R
@@ -0,0 +1,11 @@
+# Name: Hemal Karmakar
+# - Oklahoma State University
+# - Master of Science in Quantitative Finance
+# - hemal.karmakar@okstate.edu
+
+# This python class constructs portfolio of :
+#         - maximum Sharpe ratio,
+#         - minimum variance,
+#         - minimum risk at a target return level,
+#         - maximum Sharpe ratio at a target risk level &
+#         - efficient frontier.
