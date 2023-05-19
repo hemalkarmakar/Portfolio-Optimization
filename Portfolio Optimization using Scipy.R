d1aa2b6d0a6e051ce12a0c96cbd0acdfd4286eb7
Hemal Karmakar <116313075+hemalkarmakar@users.noreply.github.com>
2023-05-19 12:19:34 -0500

    Update Portfolio Optimization using Scipy.R

--- Portfolio Optimization using Scipy.R
+++ Portfolio Optimization using Scipy.R
@@ -12,10 +12,25 @@
 
 # Import libraries
 library(purrr)
+library(quantmod)
 
 # Import tickers from csv file
 tickerfilepath <- 'https://raw.githubusercontent.com/hemalkarmakar/Portfolio-Optimization/main/Portfolio%20ticker%20list.csv'
 tickerDF <- read.csv(tickerfilepath)
-ticker_list <- replace(tickerDF, tickerDF == "", NaN)
+ticker_list <- as.list(tickerDF)
+
+#getSymbols("AAPL", from = "2018-05-19", to = "2023-05-19")
+
+# Set the start and end dates
+start_date <- Sys.Date() - 5*365  # 5 years ago
+end_date <- Sys.Date()  # Today
+
+for (tic in ticker_list)
+  {
+  data = getSymbols(tic, from = start_date, to = end_date)}
+  
+  returns = data/data.shift(1)
+  self.logReturns [tic] = np.log(returns).dropna()
+  }
 
 
